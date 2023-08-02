01b253764119066c60a25aa72740d09f42e62771
vivianab45 <balleza45@gmail.com>
2023-08-02 16:39:07 -0500

    putting it together

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-//collaboration with Eric, Josh, Anthony, Richard, Daisy, Christian
+//pair programming with Eric, Josh, Anthony, Richard, Daisy, Christian, Kye
+
+
 const PersonCard = (props) => {
-    const{ firstName, lastName, age, hairColor}= props
+    const{ firstName, lastName, hairColor}= props
+        //you could also keep age in the const above and do this
+        //const[ageActual, setAgeActua]= useState(age)
+    const[age, setAge]=useState(props.age)
+
+    const addAge = () => {
+        setAge(age+1)
+        //setAgeActual(ageActual+1)
+    }
+
     return(
         <div>
-            <h1 style={{color:"red"}}>{firstName}{lastName}</h1>
+            <h1 style={{color:"red"}}>{firstName} {lastName}</h1>
             <p>Age: {age}</p>
+            {/* you would put ageActual */}
             <p>Hair Color: {hairColor}</p>
+            <button onClick={addAge}>Happy Birthday {firstName} {lastName}</button>
         </div>
     );
 }
